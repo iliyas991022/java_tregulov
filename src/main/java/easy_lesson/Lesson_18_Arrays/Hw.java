c4f27850b6a0d962220db82dc33fee252e5f1738
Iliyas Akan <ilias_98_22@inbox.ru>
2023-07-17 17:56:31 +0600

    First commit tregulov course

---
+++ src/main/java/easy_lesson/Lesson_18_Arrays/Hw.java
@@ -0,0 +1,12 @@
+package easy_lesson.Lesson_18_Arrays;
+
+public class Hw {
+    public static int[] sortirovka(int[] arr) {
+        int[] sortArr = {};
+        for(int i = 0; i < arr.length; i++) {
+
+        }
+        return arr;
+    }
+}
+
---
+++ src/main/java/easy_lesson/Lesson_18_Arrays/Hw2.java
@@ -0,0 +1,5 @@
+package easy_lesson.Lesson_18_Arrays;
+
+public class Hw2 {
+
+}
