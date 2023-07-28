607e02ab045faca3e5b6f49dd6793a5515124feb
Akan Iliyas <ilias_98_22@inbox.ru>
2023-07-28 17:55:08 +0600

    Interfaces in the callback mechanism

---
+++ src/main/java/metanit/Interfaces_in_the_callback_mechanism/EventsApp.java
@@ -0,0 +1,31 @@
+package metanit.Interfaces_in_the_callback_mechanism;
+
+public class EventsApp {
+    public static void main(String[] args) {
+        Button button= new Button (new ButtonClickHandler())    ;
+        button.click();
+        button.click();
+        button.click();
+    }
+}
+
+class ButtonClickHandler implements EventHandler{
+    public void execute(){
+        System.out.println("Кнопка нажата");
+    }
+}
+
+interface EventHandler{
+    void execute();
+
+}
+
+class Button {
+    EventHandler handler;
+    Button (EventHandler action){
+        this.handler = action;
+    }
+    public void click(){
+        handler.execute();
+    }
+}
